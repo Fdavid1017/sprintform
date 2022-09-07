3fdf14032e6713696443239547103638c14527ec
Fdavid1017 <55140434+Fdavid1017@users.noreply.github.com>
2022-09-07 23:10:59 +0200

    database base data fill

--- backend/src/main/java/com/fabry/david/sprintform/SprintformApplication.java
+++ backend/src/main/java/com/fabry/david/sprintform/SprintformApplication.java
@@ -1,24 +1,18 @@
 package com.fabry.david.sprintform;
 
 import com.fabry.david.sprintform.domains.Transaction;
-import com.fabry.david.sprintform.enums.TransactionCategory;
 import com.fabry.david.sprintform.repositories.TransactionRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.util.Date;
-import java.util.List;
+import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
 @SpringBootApplication
@@ -41,10 +35,29 @@ public class SprintformApplication implements ApplicationRunner {
         if (transactionRepository.count() == 0) {
             System.out.println("Filling base data");
 
-            transactionRepository.save(new Transaction(BigInteger.valueOf(1), "Base data 1", TransactionCategory.FINANCIAL, 1200F, "HUF", new Date()));
-            transactionRepository.save(new Transaction(BigInteger.valueOf(2), "Base data 2", TransactionCategory.HOUSING, 900F, "HUF", new Date()));
+            try {
+                Transaction[] transactions = loadBaseTransactions();
+
+                for (Transaction transaction : transactions) {
+                    transactionRepository.save(transaction);
+                }
+            } catch (com.fasterxml.jackson.core.JsonProcessingException e) {
+                System.out.println("Error while loading base datas");
+                System.out.println("==============================");
+                e.printStackTrace();
+            }
         } else {
             System.out.println("Database already has data");
         }
+    }
+
+    private Transaction[] loadBaseTransactions() throws JsonProcessingException {
+        final String uri = "https://development.sprintform.com/transactions.json";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(result, Transaction[].class);
     }
 }
